5c92d3708d18c303a2c0674cd9ef8ebf9cc08fd3
byverdu <byverdu@gmail.com>
2019-05-26 01:38:35 +0100

    linting errros

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'Theme/index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
