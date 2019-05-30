ed107ff1c0392eb4392e2ac77560333a2a9ddd00
byverdu <byverdu@gmail.com>
2019-05-30 19:54:33 +0100

    refactored folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,41 +2,41 @@ const baseConfig = require('eslint-config-react-app');
 const baseOverrides = Array.isArray(baseConfig.overrides) ? baseConfig.overrides : [baseConfig.overrides];
 const baseTsOverride = baseOverrides.find(x => x.files.find(f => f.indexOf('*.ts') > 0));
 
-module.exports ={
-    ...baseConfig,
-    overrides: [
-        {
-            ...baseTsOverride,
-            parserOptions: {
-                ecmaVersion: 2019
-            },
-            rules: {
-                ...baseTsOverride.rules,
+module.exports = {
+  ...baseConfig,
+  overrides: [
+    {
+      ...baseTsOverride,
+      parserOptions: {
+        ecmaVersion: 2019
+      },
+      rules: {
+        ...baseTsOverride.rules,
 
-                // Remove with next npm release of eslint-config-react-app:
-                'no-useless-constructor': 'off',
-                'semi': 'off',
-                'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
-                'no-alert': 'warn',
-                'no-debugger': 'warn',
-                'max-len': [ 'warn', {code: 240} ],
-                'no-multiple-empty-lines': 'warn',
-                'default-case': 'warn',
-                'comma-dangle': 'warn',
-                'no-shadow': 'warn',
-                'no-duplicate-imports': 'warn',
-                'quotes': [1, 'single'],
-                'no-dupe-class-members': 'off',
-                'triple-equals': [ true, 'allow-null-check' ],
-                '@typescript-eslint/no-useless-constructor': 'warn',
-                '@typescript-eslint/no-unused-vars': 'warn',
-                '@typescript-eslint/semi': ['warn']
-                // << add your own custom rules here >>
-            },
-        }
-    ],
-    rules: {
-        ...baseConfig.rules,
+        // Remove with next npm release of eslint-config-react-app:
+        'no-useless-constructor': 'off',
+        'semi': 'off',
+        'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+        'no-alert': 'warn',
+        'no-debugger': 'warn',
+        'max-len': ['warn', { code: 240 }],
+        'no-multiple-empty-lines': 'warn',
+        'default-case': 'warn',
+        'comma-dangle': 'warn',
+        'no-shadow': 'warn',
+        'no-duplicate-imports': 'warn',
+        'quotes': [1, 'single'],
+        'no-dupe-class-members': 'off',
+        'triple-equals': [true, 'allow-null-check'],
+        '@typescript-eslint/no-useless-constructor': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/semi': ['warn']
         // << add your own custom rules here >>
-    },
+      },
+    }
+  ],
+  rules: {
+    ...baseConfig.rules,
+    // << add your own custom rules here >>
+  },
 };
--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,12 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const {
   override,
   useEslintRc,
   addWebpackAlias
 } = require('customize-cra');
 
-// module.exports = function override(config, env) {
-//     if (env === 'development') {
-//         config.resolve.alias["react-dom"] = "@hot-loader/react-dom";
-//     }
-//     config = rewireReactHotLoader(config, env);
-//     return config;
-// };
-
 module.exports = override(
   useEslintRc(),
   addWebpackAlias({
-    "react-dom": "@hot-loader/react-dom"
+      "react-dom": "@hot-loader/react-dom"
   }),
 );
