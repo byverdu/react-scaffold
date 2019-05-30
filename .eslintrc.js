6644c1ef7a5802c20e6bb218faffc17deab48c69
byverdu <byverdu@gmail.com>
2019-05-30 19:43:08 +0100

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,36 @@
-// .eslintrc.js
-'use strict';
-
 const baseConfig = require('eslint-config-react-app');
 const baseOverrides = Array.isArray(baseConfig.overrides) ? baseConfig.overrides : [baseConfig.overrides];
 const baseTsOverride = baseOverrides.find(x => x.files.find(f => f.indexOf('*.ts') > 0));
 
-module.exports = {
+module.exports ={
     ...baseConfig,
     overrides: [
         {
             ...baseTsOverride,
+            parserOptions: {
+                ecmaVersion: 2019
+            },
             rules: {
                 ...baseTsOverride.rules,
 
                 // Remove with next npm release of eslint-config-react-app:
-                'default-case': 'off',
                 'no-useless-constructor': 'off',
+                'semi': 'off',
+                'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+                'no-alert': 'warn',
+                'no-debugger': 'warn',
+                'max-len': [ 'warn', {code: 240} ],
+                'no-multiple-empty-lines': 'warn',
+                'default-case': 'warn',
+                'comma-dangle': 'warn',
+                'no-shadow': 'warn',
+                'no-duplicate-imports': 'warn',
+                'quotes': [1, 'single'],
+                'no-dupe-class-members': 'off',
+                'triple-equals': [ true, 'allow-null-check' ],
                 '@typescript-eslint/no-useless-constructor': 'warn',
                 '@typescript-eslint/no-unused-vars': 'warn',
-                "semi": "off",
-                "@typescript-eslint/semi": ["error"],
-                'no-dupe-class-members': 'off',
-
+                '@typescript-eslint/semi': ['warn']
                 // << add your own custom rules here >>
             },
         }
