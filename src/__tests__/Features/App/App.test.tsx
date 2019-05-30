324c3c98229d6d7771d25a16c119328e79ce0f36
byverdu <byverdu@gmail.com>
2019-05-30 23:13:57 +0100

    setup for enzyme test

--- src/__tests__/Features/App/App.test.tsx
+++ src/__tests__/Features/App/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'Features/App/App';
+import enzyme, { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+	let wrapper: enzyme.ShallowWrapper;
+
+	beforeEach(() => {
+		wrapper = shallow(<App />);
+	});
+
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it('should create a snapshot', () => {
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
 });
