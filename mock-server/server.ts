74aa3fd3783c3b6b90f58374b43d9ea60153cc70
byverdu <byverdu@gmail.com>
2019-06-04 00:36:34 +0100

    added json-server using typescript

---
+++ mock-server/server.ts
@@ -0,0 +1,98 @@
+import { Application, Response, Request } from "express-serve-static-core";
+const jsonServer = require("json-server");
+import { utils } from './utils';
+
+const {
+  getContentForRoutes,
+  addTodo,
+  updateTodo,
+  deleteTodo
+} = utils;
+
+const PORT = process.env.PORT || 3001;
+
+function createApp(routes) {
+  const server: Application = jsonServer.create();
+  const router = jsonServer.router(routes);
+  const db = router.db // lowdb instance
+
+  db._.mixin({
+    addTodo,
+    updateTodo,
+    deleteTodo
+  })
+
+  const middlewares = jsonServer.defaults();
+  server.use(middlewares);
+  server.use(jsonServer.bodyParser);
+
+  server.get("/todos", (req: Request, res: Response) => {
+    const todos = db.get('todos').value();
+    res.status(200);
+    
+    res.jsonp(todos);
+  });
+
+  server.post("/todos/add", (req: Request, res: Response) => {
+    const { id, todo } = req.body;
+    const todos = db.get('todos')
+      .addTodo(id, todo)
+      .value();
+    
+    res.status(200);
+
+    db.set('todos', todos).write();
+    
+    res.jsonp(todos)
+  });
+
+  server.patch("/todos/update", (req: Request, res: Response) => {
+    const { id, status } = req.body;
+    const todos = db.get('todos')
+      .updateTodo(id, status)
+      .value();
+    
+    res.status(200);
+
+    db.set('todos', todos).write();
+    
+    res.jsonp(todos)
+  });
+
+  server.delete("/todos/delete/:todoId", (req: Request, res: Response) => {
+    const todoId = req.param('todoId');
+    const todos = db.get('todos')
+      .deleteTodo(todoId)
+      .value();
+    
+    res.status(200);
+
+    db.set('todos', todos).write();
+    
+    res.jsonp(todos)
+  });
+
+  server.use((req, res, next) => {
+    if (req.method === "GET") {
+      // Clearing query strings, so the filter feature does not kick in
+      req.query = {};
+    }
+    // Continue to JSON Server router
+    next();
+  });
+
+  server.use(router);
+
+  return server;
+}
+
+(async () => {
+  try {
+    const routes = await getContentForRoutes();
+    const app = createApp(routes);
+    console.log(`app running on port ${PORT}`);
+    app.listen(PORT);
+  } catch (error) {
+    throw new Error(error)
+  }
+})()
---
+++ mock-server/utils.ts
@@ -0,0 +1,64 @@
+import { Todo } from '../src/Models/Todo';
+const { promisify } = require('util');
+const path = require("path");
+const fs = require("fs");
+
+interface Utils {
+  getContentForRoutes: () => Promise<any>;
+  addTodo: (todos: { [key: string]: Todo; }, id: string, todo: Todo) => { [key: string]: Todo; };
+  updateTodo: (todos: { [key: string]: Todo; }, id: string, status: boolean) => { [key: string]: Todo; };
+  deleteTodo: (todos: { [key: string]: Todo; }, id: string) => { [key: string]: Todo; };
+}
+
+const readFileAsync = promisify(fs.readFile);
+const getPathToJsonMock = fileName => {
+  const mocksPath = 'data';
+  const jsonPath = path.join(__dirname, '../', `${mocksPath}`, `${fileName}.json`);
+
+  return jsonPath;
+};
+
+export const utils: Utils = {
+  addTodo: (todos: { [key: string]: Todo; }, id: string, todo: Todo) => {
+    todos[id] = todo;
+  
+    return todos;
+  },
+  updateTodo: (todos: { [key: string]: Todo; }, id: string, status: boolean) => {
+    todos[id].done = status;
+  
+    return todos;
+  },
+  deleteTodo: (todos: { [key: string]: Todo; }, id: string) => {
+    console.log(todos, id, 'before');
+    delete todos[id];
+    console.log(todos, 'after');
+  
+    return todos;
+  },
+  getContentForRoutes: () => {
+    let count = 0;
+    const paths = {};
+    const mapRoutesToJson = {
+      todos: 'todos'
+    };
+    const routes = Object.keys(mapRoutesToJson);
+
+    return new Promise((resolve, reject) => {
+      routes.forEach(async route => {
+        const fileName = mapRoutesToJson[route];
+
+        try {
+          const fileContent = await readFileAsync(getPathToJsonMock(fileName), { encoding: 'utf8' })
+          paths[route] = JSON.parse(fileContent)
+          count += 1;
+          if (count === routes.length) {
+            resolve(paths);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+    })
+  }
+}
