8bf4b253242f6f5966e9f4adc23f3df4ad7d268c
Albert Vallverdu <albert.vallverdu@sky.uk>
2019-06-07 13:21:19 +0100

    solved linting issues

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,8 @@
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import MockAdapter from 'axios-mock-adapter';
+import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 
 enzyme.configure({ adapter: new Adapter() });
 
-// export const axiosMock = new MockAdapter(axios);
+export const axiosMock = new MockAdapter(axios);
