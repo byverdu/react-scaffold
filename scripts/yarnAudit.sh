30f424571c982847b2cb31462b58a9484c7b230a
byverdu <byverdu@gmail.com>
2019-06-10 23:35:15 +0100

    change sh script to be runnable in travis

--- scripts/yarnAudit.sh
+++ scripts/yarnAudit.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # run yarn audit, check the number of lines that have 'high' on
 # them in the output table, meaning high|critical vulnerability
-if [[ $(yarn audit | grep -E "high | critical" | wc -l | tr -d ' ') -gt 0 ]]; then
+if [ $(yarn audit | grep -E "high | critical" | wc -l | tr -d ' ') -gt 0 ]; then
   echo "\x1b[91mhigh or critical vulnerability found in your dependencies\x1b[0m"
   exit 1
 fi
