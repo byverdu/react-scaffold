66ac03fd855dbb106a2900261e0ba5f31d9c9c29
byverdu <byverdu@gmail.com>
2019-06-10 23:21:36 +0100

    refactored yarn audit command

--- scripts/yarnAudit.sh
+++ scripts/yarnAudit.sh
@@ -1,6 +1,8 @@
 # run yarn audit, check the number of lines that have 'high' on
 # them in the output table, meaning high|critical vulnerability
-if [[ $(yarn audit | grep -E "high | critical" | wc -l | tr -d ' ') -gt 0 ]]; then
-  echo "high or critical vulnerability found in your dependencies"
+if [[ $( yarn audit | grep -E "high | critical" | wc -l | tr -d ' ') -gt 0 ]]; then
+  echo "\x1b[91mhigh or critical vulnerability found in your dependencies\x1b[0m"
   exit 1
 fi
+
+echo "\x1b[32mNo vulnerabilities found\x1b[0m"
