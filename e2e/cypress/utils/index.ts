9af6b6abe4c21efb76815267a063087d0dbf49be
byverdu <byverdu@gmail.com>
2019-06-14 01:20:32 +0100

    refactored cypress and added e2e for add todo

--- e2e/cypress/utils/index.ts
+++ e2e/cypress/utils/index.ts
@@ -4,4 +4,12 @@ export default class HomePage {
   visit() {
     cy.visit('http://localhost:3000/');
   }
+
+  fillAddTodoInput(todoText: string) {
+    cy.get('#add-todo-field').type(todoText);
+  }
+
+  submitAddTodoForm() {
+    cy.get('.add-todo button').click();
+  }
 }
--- e2e/cypress/utils/routes.ts
+++ e2e/cypress/utils/routes.ts
@@ -1,4 +1,6 @@
 /// <reference types="Cypress" />
+import {Todo} from '../../../src/Models/Todo';
+import {Map} from 'immutable';
 
 export default class TodosRoutes {
   getTodos(emptyResponse: boolean = false) {
@@ -9,6 +11,19 @@ export default class TodosRoutes {
           status: 200,
           response: emptyResponse ? {} : todos
       }).as('getTodos');
+    });
+  }
+
+  addTodo(todo: Todo) {
+    cy.fixture('todos.json').then((todos: Map<string, Todo>) => {
+      const newTodos = Map(todos).set(todo.id, todo);
+      console.log(todos, newTodos)
+      cy.route({
+          method: 'POST',
+          url: '/todos/add',
+          status: 200,
+          response: newTodos
+      }).as('addTodo');
     });
   }
 }
