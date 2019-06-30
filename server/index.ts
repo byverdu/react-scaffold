f4bc0c69d59c7866cf82ed99153ec59309aab61f
byverdu <byverdu@gmail.com>
2019-07-01 00:55:57 +0100

    added controllers for post and delete

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,9 @@ import path from 'path';
 import {
   getRoot,
   getTodos,
-  // postTodosAdd,
-  // deleteTodos,
-  // putTodosUpdate
+  postTodosAdd,
+  deleteTodos,
+  putTodosUpdate
 } from './controllers';
 
 const express = require('express');
@@ -19,9 +19,9 @@ app.use('/static', express.static(path.join(__dirname, "/client/static")));
 
 app.get('/', getRoot);
 app.get('/todos', getTodos);
-// app.post('/todos/add', postTodosAdd);
-// app.delete('/todos/delete/:todoId', deleteTodos);
-// app.patch('/todos/update', putTodosUpdate);
+app.post('/todos/add', postTodosAdd);
+app.delete('/todos/delete/:todoId', deleteTodos);
+app.patch('/todos/update', putTodosUpdate);
 
 
 app.listen(PORT, function () {
