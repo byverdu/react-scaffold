070c3bb2e838364d712f601433cd283f39926197
byverdu <byverdu@gmail.com>
2019-07-01 00:48:26 +0100

    added controllers for getRoot and getTodos

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,28 @@
-import { Application, Request, Response } from 'express';
+import { Application } from 'express';
+
 import path from 'path';
+import {
+  getRoot,
+  getTodos,
+  // postTodosAdd,
+  // deleteTodos,
+  // putTodosUpdate
+} from './controllers';
 
 const express = require('express');
+var bodyParser = require('body-parser');
 const app: Application = express();
 const PORT = process.env.PROT || 3001;
 
-app.use('/static', express.static(path.join(__dirname, "/client/static")))
+app.use(bodyParser.json());
+app.use('/static', express.static(path.join(__dirname, "/client/static")));
 
-app.get('/todos', function (req: Request, res: Response) {
-  res
-    .status(200)
-    .json({
-      '1234': {
-        id: '1234',
-        text: 'alohhaa',
-        done: false
-      }
-    });
-});
+app.get('/', getRoot);
+app.get('/todos', getTodos);
+// app.post('/todos/add', postTodosAdd);
+// app.delete('/todos/delete/:todoId', deleteTodos);
+// app.patch('/todos/update', putTodosUpdate);
 
-const jsonPath = path.join(__dirname, 'client/index.html');
-
-app.get('/', function (req, res) {
-  res.status(200).sendFile( jsonPath );
-});
 
 app.listen(PORT, function () {
   console.info(`Example app listening on port ${PORT}!`);
