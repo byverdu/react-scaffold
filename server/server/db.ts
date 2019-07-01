85206991e5cd49a5ef8882d16dc35082b6ac423f
byverdu <byverdu@gmail.com>
2019-07-02 00:12:51 +0100

    added tests for /todos/add endpoint

--- server/server/db.ts
+++ server/server/db.ts
@@ -1,7 +1,15 @@
-import level, {LevelUp} from 'level';
 import path from 'path';
 
-const dbPath = process.env.DB_PATH || path.join(__dirname, 'todosDb');  
-const db: LevelUp = level(dbPath);
+const dbPath = process.env.DB_PATH || path.join(__dirname, 'todosDb');
+let level;
+let db;
+
+if (process.env.NODE_ENV === 'test') {
+  level = require('level-test')({ mem: true })
+  db = level({ valueEncoding: 'json' })
+} else {
+  level = require('level');
+  db = level(dbPath)
+}
 
 export default db;
