4f5cd18eec6af68398ebc8b168de8459082fa68f
byverdu <byverdu@gmail.com>
2019-07-11 00:07:45 +0100

    Readme updated

--- scripts/printTreeView.sh
+++ scripts/printTreeView.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if tree ; then # TRY
-  tree -I 'node_modules|coverage|build|__*|public' > structure.md
+  tree -I 'node_modules|coverage|build|__*|public|app|build-server' > structure.md
 else # CATCH
     echo -e "\x1b[91mtree command is not installed, try to run \"brew install tree\"\x1b[0m"
 fi
